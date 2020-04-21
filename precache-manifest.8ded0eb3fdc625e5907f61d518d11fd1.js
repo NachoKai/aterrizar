93784d8462b0283ba933f47c71c3398387632bf0
NachoKai <nachokai@outlook.com>
2020-04-21 13:56:29 -0300

    Updates

---
+++ precache-manifest.8ded0eb3fdc625e5907f61d518d11fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aace428be60aa3677e8ff8261797bf",
+    "url": "/aterrizar/index.html"
+  },
+  {
+    "revision": "d30413baf7cd1ca0dd74",
+    "url": "/aterrizar/static/css/main.ac1eddd4.chunk.css"
+  },
+  {
+    "revision": "c9e798a97ad799b16800",
+    "url": "/aterrizar/static/js/2.28560e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aterrizar/static/js/2.28560e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30413baf7cd1ca0dd74",
+    "url": "/aterrizar/static/js/main.85aa353c.chunk.js"
+  },
+  {
+    "revision": "1297772cbb79f208c799",
+    "url": "/aterrizar/static/js/runtime-main.a9a91b66.js"
+  }
+]);
